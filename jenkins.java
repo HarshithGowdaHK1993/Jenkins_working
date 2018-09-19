0bb56be78bef65735b004f940f1583003b0f83a8
HarshithGowdahk <Harshith.Kalegowda@aptean.com>
2018-09-19 16:35:05 +0530

    Harshith gowda is committing

---
+++ jenkins.java
@@ -0,0 +1,12 @@
+
+public class jenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i=1;i<=10;i++)
+	System.out.println(i);
+	}
+
+}
+///Jenkins/src/jenkins.java
+//C:\Users\hkalegowda\workspace\Jenkins\src\jenkins.java

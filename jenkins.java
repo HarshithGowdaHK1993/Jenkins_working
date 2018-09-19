9d1c775f57c2878871e69e5261b1c51a02d0cc49
HarshithGowdahk <Harshith.Kalegowda@aptean.com>
2018-09-19 16:40:34 +0530

    Made changes

--- jenkins.java
+++ jenkins.java
@@ -3,10 +3,17 @@ public class jenkins {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-for (int i=1;i<=10;i++)
+for (int i=1;i<=5;i++)
 	System.out.println(i);
 	}
 
 }
 ///Jenkins/src/jenkins.java
 //C:\Users\hkalegowda\workspace\Jenkins\src\jenkins.java
+
+
+
+//git init
+//git status
+//git add FILE_NAME
+//git commit -m "Message"
